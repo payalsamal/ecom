bd9f27e125ced7ef74b6b5c83a2836462cab8e68
payalsamal <samalpayal03@gmail.com>
2026-03-22 20:02:04 +0530

    pagination and cros config

--- src/main/java/com/example/ecom/product/service/ProductService.java
+++ src/main/java/com/example/ecom/product/service/ProductService.java
@@ -3,55 +3,58 @@ package com.example.ecom.product.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.ecom.product.entity.Product;
 import com.example.ecom.product.model.ProductDto;
+import com.example.ecom.product.model.ProductList;
 import com.example.ecom.product.repository.ProductRepository;
-
-
-
+import org.springframework.data.domain.Page;
 @Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	public void saveProduct(ProductDto productDto) {
-	Product prod=	productRepository.save(mapDtoToEntity(productDto));
-	System.out.println(prod.toString());
+		Product prod = productRepository.save(mapDtoToEntity(productDto));
+		System.out.println(prod.toString());
 	}
-	
-	Product mapDtoToEntity(ProductDto dto)
-	{
-		Product prod = Product.builder()
-				.category(dto.category())
-				.inStock(dto.inStock())
-				.name(dto.name())
-				.price(dto.price())
-				.build();
+
+	Product mapDtoToEntity(ProductDto dto) {
+		Product prod = Product.builder().category(dto.category()).inStock(dto.inStock()).name(dto.name())
+				.price(dto.price()).build();
 		return prod;
 	}
-	
+
 	public List<ProductDto> getAll() {
-		
-			return (productRepository.findAll().stream().map(this::mapEntityToDto).toList());
-		
-		}
-	ProductDto mapEntityToDto(Product dto)
-	{
-		ProductDto prod = ProductDto.builder()
-				.category(dto.getCategory())
-				.inStock(dto.isInStock())
-				.name(dto.getName())
-				.price(dto.getPrice())
-				.id(dto.getId())
-				.build();
+
+		return (productRepository.findAll().stream().map(this::mapEntityToDto).toList());
+
+	}
+
+	ProductDto mapEntityToDto(Product dto) {
+		ProductDto prod = ProductDto.builder().category(dto.getCategory()).inStock(dto.isInStock()).name(dto.getName())
+				.price(dto.getPrice()).id(dto.getId()).build();
 		return prod;
 	}
+
 	public List<ProductDto> getAllInstock() {
-		
+
 		return (productRepository.findAll().stream().map(this::mapEntityToDto).filter(ProductDto::inStock).toList());
-	
+
 	}
+
+	public ProductList getAllWithPagination(Integer pageNo, Integer pageSize) {
+		Pageable page = PageRequest.of(pageNo, pageSize);
+		
+		Page<Product> data = productRepository.findAll(page);
+		return  ProductList.builder().products( data.getContent().stream().map(this::mapEntityToDto).toList())
+				.totalPage(data.getTotalPages()).build()
+				;
+
+	}
+
 }
